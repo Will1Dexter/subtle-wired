aa3df4a407ac643cb8cf1dc21bacad959859a64a
Willian Antonio dos Santos <will1.scientist@gmail.com>
2025-05-21 02:15:56 -0300

    refactor: change in the name of the components in the container

--- simple_dependency_injector/core/container.py
+++ simple_dependency_injector/core/container.py
@@ -9,18 +9,18 @@ from simple_dependency_injector.core.injector import Injector
 
 
 class Container(ABC):
-    implementations: list[ContainerComponent] = []
+    components: list[ContainerComponent] = []
 
     def startup(self):
-        for impl in self.__class__.implementations:
-            if not hasattr(impl, "container_config"):
+        for comp in self.__class__.components:
+            if not hasattr(comp, "container_config"):
                 raise AttributeError(
-                    f"The class '{impl.__name__}' must be decorated with a singleton or factory to be used in the container!"
+                    f"The class '{comp.__name__}' must be decorated with a singleton or factory to be used in the container!"
                 )
 
-            config = impl.container_config()
+            config = comp.container_config()
             Injector().upsert_implementation(
-                impl,
+                comp,
                 injected_type=config.get("injected_type", "singleton"),
                 is_lazy=config.get("is_lazy", False),
             )
